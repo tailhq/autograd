67997adbe31dbc12cd959abf5ac5a89e1c07d55d
kogecoo <kogecoo@gmail.com>
2015-10-12 02:46:15 +0900

    Add test for Neg.

--- src/test/scala/com/kogecoo/scalaad/graph/OpSpec.scala
+++ src/test/scala/com/kogecoo/scalaad/graph/OpSpec.scala
@@ -378,4 +378,34 @@ class OpSpec extends FunSuite {
     a2.propagate(cValue3_5) shouldBe Seq(3.0f, 5f)
 
   }
+
+  test("Neg - Seq[Float]") {
+    implicit val vr = ValueRuleExample.valueRuleSeqFloat
+
+    val var71_3 = Var[Seq, Float](Seq(71.0f, 3f))
+    val c12_42 = ContainerConst[Seq, Float](Seq(12.0f, 42.0f))
+    val sc2 = ScalarConst(2.0f)
+
+    val value15 = NonContainerValue[Seq, Float](15.0f)
+    val cValue3_5 = ContainerValue[Seq, Float](Seq(3.0f, 5.0f))
+
+    // -ContainerConst
+    val a1 = Neg(c12_42)
+
+    a1.apply()              shouldBe Seq(-12f , -42f)
+    a1.deriv(var71_3)       shouldBe 0f
+    a1.deriv(c12_42)        shouldBe 0f
+    a1.propagate(value15)   shouldBe 0f
+    a1.propagate(cValue3_5) shouldBe Seq(0.0f, 0.0f)
+
+    // -Var
+    val a2 = Neg(var71_3)
+
+    a2.apply()              shouldBe Seq(-71f , -3f)
+    a2.deriv(var71_3)       shouldBe -1f
+    a2.deriv(c12_42)        shouldBe 0f
+    a2.propagate(value15)   shouldBe -15f
+    a2.propagate(cValue3_5) shouldBe Seq(-3.0f, -5f)
+
+  }
 }
