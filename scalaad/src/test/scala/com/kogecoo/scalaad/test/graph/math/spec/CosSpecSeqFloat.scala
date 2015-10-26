a5ca0063741fa5bcabb8fbd0e213faa2887019e0
kogecoo <kogecoo@gmail.com>
2015-10-27 01:58:41 +0900

    Test: Add tests for cos.

---
+++ scalaad/src/test/scala/com/kogecoo/scalaad/test/graph/math/spec/CosSpecSeqFloat.scala
@@ -0,0 +1,68 @@
+package com.kogecoo.scalaad.test.graph.math.spec
+
+import com.kogecoo.scalaad.graph.{cos, Node}
+import com.kogecoo.scalaad.rule._
+import com.kogecoo.scalaad.test.helper.gen._
+import com.kogecoo.scalaad.test.helper.rule.SeqFloatSoftCompareRule
+import com.kogecoo.scalaad.test.helper.rule.SeqFloatValueRule.Implicits._
+import com.kogecoo.scalaad.test.helper.specgen.{UnaryOpSpec, UnaryOpExpectedBehaviorDef}
+import org.scalacheck.Properties
+
+import scala.language.higherKinds
+
+
+object CosSpecSeqFloat extends Properties("Cos - Seq[Float]") {
+
+  implicit val compareRule = new SeqFloatSoftCompareRule
+
+  val nodeGen  = new SeqFloatNodeGen
+  val valueGen = new SeqFloatValueGen
+  val expects  = new CosSeqFloatExpectedBehavior
+
+  val seqFloatSpecGen = new UnaryOpSpec[Seq, Float](expects, nodeGen, valueGen)
+
+  property("cos(a) apply") = seqFloatSpecGen.applyScalar()
+  property("cos(a) apply") = seqFloatSpecGen.applyContainer()
+  property("cos(a) apply") = seqFloatSpecGen.applyVar()
+
+  property("cos(a) (scalar) w.r.t. a")    = seqFloatSpecGen.derivScalarWrtSelf()
+  property("cos(a) (scalar) w.r.t. b")    = seqFloatSpecGen.derivScalarWrtUnknown()
+  property("cos(a) (container) w.r.t. a") = seqFloatSpecGen.derivContainerWrtSelf()
+  property("cos(a) (container) w.r.t. b") = seqFloatSpecGen.derivContainerWrtUnknown()
+  property("cos(a) (var) w.r.t. a")       = seqFloatSpecGen.derivContainerWrtSelf()
+  property("cos(a) (var) w.r.t. b")       = seqFloatSpecGen.derivContainerWrtUnknown()
+
+  property("cos(a) (scalar) propagete with value")        = seqFloatSpecGen.propagateScalarWithNCValue()
+  property("cos(a) (scalar) propagate with container")    = seqFloatSpecGen.propagateScalarWithCValue()
+  property("cos(a) (container) propagete with value")     = seqFloatSpecGen.propagateContainerWithNCValue()
+  property("cos(a) (container) propagate with container") = seqFloatSpecGen.propagateContainerWithCValue()
+  property("cos(a) (container) propagete with value")     = seqFloatSpecGen.propagateVarWithNCValue()
+  property("cos(a) (container) propagate with container") = seqFloatSpecGen.propagateVarWithCValue()
+
+  property("cos(a) grad")              = seqFloatSpecGen.gradScalar()
+  property("cos(a) grad")              = seqFloatSpecGen.gradContainer()
+  property("cos(a) grad")              = seqFloatSpecGen.gradVar()
+
+}
+
+
+class CosSeqFloatExpectedBehavior(implicit vr: MathRule[Seq, Float]) extends UnaryOpExpectedBehaviorDef[Seq, Float] {
+
+  override val zero: Float = 0f
+
+  override def zero(shape: Seq[Float]): Seq[Float] = Seq.fill(shape.size)(0f)
+
+  override def op(node: Node[Seq, Float]): Node[Seq, Float] = cos(node)
+
+  override def applyScalar(a: Float): Float              = scala.math.cos(a).toFloat
+  override def applyContainer(a: Seq[Float]): Seq[Float] = a.map { x => scala.math.cos(x.toDouble).toFloat }
+  override def applyVar(a: Seq[Float]): Seq[Float]       = a.map { x => scala.math.cos(x.toDouble).toFloat }
+
+  override def derivVarWrtSelf(a: Seq[Float]): Seq[Float] = a.map { x => -scala.math.sin(x.toDouble).toFloat }
+
+  override def propagateVarWithNCValue(a: Seq[Float], b: Float): Seq[Float] = a.map { x => -b * scala.math.sin(x.toDouble).toFloat }
+  override def propagateVarWithCValue(a: Seq[Float], b: Seq[Float]): Seq[Float] = a.zip(b).map { case (x, y) => -y * scala.math.sin(x.toDouble).toFloat }
+
+  override def gradVar(a: Seq[Float]): Seq[Float] = a.map { x => -scala.math.sin(x.toDouble).toFloat }
+
+}
