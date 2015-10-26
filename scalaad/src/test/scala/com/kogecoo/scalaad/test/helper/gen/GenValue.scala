2169b43443a3b6005bc650ed0432874b6b98a3e0
kogecoo <kogecoo@gmail.com>
2015-10-27 03:28:48 +0900

    Test: toString for generated node/value samples

--- scalaad/src/test/scala/com/kogecoo/scalaad/test/helper/gen/GenValue.scala
+++ scalaad/src/test/scala/com/kogecoo/scalaad/test/helper/gen/GenValue.scala
@@ -29,6 +29,10 @@ abstract class GenValue[U[_], T] {
 
 }
 
-class NonContainerValueSample[U[_], T](val value: NonContainerValue[U, T], val src: T)
+class NonContainerValueSample[U[_], T](val value: NonContainerValue[U, T], val src: T) {
+  override def toString = s"NonContainerValueSample(${value}, ${src})"
+}
 
-class ContainerValueSample[U[_], T](val value: ContainerValue[U, T], val src: U[T])
+class ContainerValueSample[U[_], T](val value: ContainerValue[U, T], val src: U[T]) {
+  override def toString = s"ContainerValueSample(${value}, ${src})"
+}
